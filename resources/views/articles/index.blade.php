208ab5bd49000191855c0bcef6f55b03d5c38925
JehanKandy <jehankandy@gmail.com>
2023-06-27 07:21:40 +0530

    Update

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,11 @@
+@php
+    if(Auth::user()->role == 1){
+        echo "@extends('layouts.admin_dash')";
+    }
+    elseif (Auth::user()->role == 2) {
+        echo "@extends('layouts.staff_dash')";
+    }
+    elseif (Auth::user()->role == 3) {
+        echo "@extends('layouts.client_dash')";
+    }
+@endphp
