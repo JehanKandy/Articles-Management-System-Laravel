02d7b2643c9577558f8fcdc45169ffa025bfb88e
JehanKandy <jehankandy@gmail.com>
2023-06-28 22:15:57 +0530

    Update

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -39,10 +39,10 @@
                       <td>{{ $article->created_at }}</td>
                       <td>{{ $article->updated_at }}</td>
                       <td>
-                        <a href="{{ url('/articles/' . $item->id) }}" title="View Articles"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                        <a href="{{ url('/articles/' . $item->id . '/edit') }}" title="Edit Articles"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        <a href="{{ url('/articles/' . $article->id) }}" title="View Articles"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                        <a href="{{ url('/articles/' . $article->id . '/edit') }}" title="Edit Articles"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                        <form method="POST" action="{{ url('/articles' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                        <form method="POST" action="{{ url('/articles' . '/' . $article->id) }}" accept-charset="UTF-8" style="display:inline">
                             {{ method_field('DELETE') }}
                             {{ csrf_field() }}
                             <button type="submit" class="btn btn-danger btn-sm" title="Delete Articles" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
