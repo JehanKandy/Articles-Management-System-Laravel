ff3fe1914e70f8abc4b976d974a7718c41b2f2a2
JehanKandy <jehankandy@gmail.com>
2023-06-28 18:25:51 +0530

    Update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Dashboard</title>
+    @php
+        if (condition) {
+            # code...
+        }
+    @endphp
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
