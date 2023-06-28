e6fe998fbdc19f43b90a9b4aa43e0ae097f23c75
JehanKandy <jehankandy@gmail.com>
2023-06-28 19:32:37 +0530

    Update

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -13,6 +13,17 @@
             <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a><br><br>
             <h3>Create New Article</h3>
             <hr>
+            <form action="{{ url('articles') }}" method="POST" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="add_user">Owner</label>
+                        @php
+                            $user = Auth::user()->name;
+                        @endphp
+                        <input type="text" name="add_user" value=" @php echo Auth::user()->name; @endphp" class="form-control" disabled>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>    
 </div>
