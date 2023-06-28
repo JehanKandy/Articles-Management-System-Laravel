a0aae347a827ffbe4228051aefe4538294c52d7e
JehanKandy <jehankandy@gmail.com>
2023-06-28 18:04:14 +0530

    Update

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,28 +1,18 @@
-{{-- @php
-    if(Auth::user()->role == 1){
-         
-    }
-    elseif (Auth::user()->role == 2) {
-        echo "@extends('layouts.staff_dash')";
-    }
-    elseif (Auth::user()->role == 3) {
-        echo "@extends('layouts.client_dash')";
-    }
-@endphp --}}
 
-@if (Auth::user()->role == 1)
+
+{{-- @if(Auth::user()->role == 1)
     @extends('layouts.admin_dash')
-@endif
-
-@if (Auth::user()->role == 2)
+@elseif (Auth::user()->role == 2)
     @extends('layouts.staff_dash')
-@endif
+@endif --}}
 
-@if (Auth::user()->role == 3)
-    @extends('layouts.client_dash')
-@endif
+@php
+    if(Auth::user()->role == 1){
+       Blade::include('layouts.admin_dash');
+    }
+@endphp
 
-
+@include('layouts.header')
 
 @section('content')
     hi all
