3766e1633ce3b8d7e3c93c5c153fe0f7a2009efe
JehanKandy <jehankandy@gmail.com>
2023-06-28 19:13:06 +0530

    Update

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,14 +1,16 @@
 @include('layouts.header')
 
 <style>
-    *{
+    .article-content{
         background-color: #c1e0ef;
     }
 </style>
 
-<div class="container">
-    <div class="admin-card">
-        <h3>Create New Article</h3>
-        <hr>
-    </div>
+<div class="article-content">
+    <div class="container">
+        <div class="admin-card">
+            <h3>Create New Article</h3>
+            <hr>
+        </div>
+    </div>    
 </div>
