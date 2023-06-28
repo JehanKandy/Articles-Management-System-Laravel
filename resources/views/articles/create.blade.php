1dd6a5ba471d15915a438da69be06e2e1ad10f35
JehanKandy <jehankandy@gmail.com>
2023-06-28 19:15:11 +0530

    Update

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,7 +10,7 @@
 <div class="article-content">
     <div class="container">
         <div class="admin-card">
-            <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a>
+            <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a><br><br>
             <h3>Create New Article</h3>
             <hr>
         </div>
