044a1d877f6540a1c9151aa88c8ef5bd64c1099c
JehanKandy <jehankandy@gmail.com>
2023-06-28 19:12:23 +0530

    Update

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,1 +1,14 @@
 @include('layouts.header')
+
+<style>
+    *{
+        background-color: #c1e0ef;
+    }
+</style>
+
+<div class="container">
+    <div class="admin-card">
+        <h3>Create New Article</h3>
+        <hr>
+    </div>
+</div>
