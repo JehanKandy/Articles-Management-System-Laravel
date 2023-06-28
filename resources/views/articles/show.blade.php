2941f6f3c17bf0e2260ba50c6e0ef6f2f51feaa9
JehanKandy <jehankandy@gmail.com>
2023-06-28 22:22:41 +0530

    Update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,3 +6,10 @@
         padding: 20px 0;
     }
 </style>
+<div class="article-content">
+    <div class="container">
+        <div class="admin-card">
+            <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a><br><br>
+        </div>
+    </div>
+</div>
