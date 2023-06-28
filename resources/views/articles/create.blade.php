d07e1a7a46d935058d0d05ac2d7c6dc44835adc9
JehanKandy <jehankandy@gmail.com>
2023-06-28 19:58:23 +0530

    Update

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -36,8 +36,8 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-12">
-                        <label for="article_data">Article Tags</label>
-                        <textarea name="article_data" cols="30" rows="10" class="form-control" style="resize: none;"></textarea><br>
+                        <label for="tags">Article Tag</label>
+                        <input type="text" name="tags" class="form-control"><br>
                     </div>
                 </div>
                 <div class="row">
