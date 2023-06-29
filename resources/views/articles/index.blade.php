54c7f6d9432721cd59f84abf37d940c5e4504e1b
JehanKandy <jehankandy@gmail.com>
2023-06-29 07:34:13 +0530

    Update

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -40,13 +40,18 @@
                       <td>{{ $article->updated_at }}</td>
                       <td>
                         <a href="{{ url('/articles/' . $article->id) }}" title="View Articles"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                        <a href="{{ url('/articles/' . $article->id . '/edit') }}" title="Edit Articles"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                        <form method="POST" action="{{ url('/articles' . '/' . $article->id) }}" accept-charset="UTF-8" style="display:inline">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Articles" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                        </form>
+                        @if (Auth::user->role == 1 || Auth::user->name == $article->add_user)
+                            <a href="{{ url('/articles/' . $article->id . '/edit') }}" title="Edit Articles"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                            <form method="POST" action="{{ url('/articles' . '/' . $article->id) }}" accept-charset="UTF-8" style="display:inline">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Articles" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            </form>
+                        @endif
+
+
                       </td>
 
                   </tr>
