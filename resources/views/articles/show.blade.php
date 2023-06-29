5fb6569456d20a87e484a3894868cbb76215d9b9
JehanKandy <jehankandy@gmail.com>
2023-06-29 06:23:20 +0530

    Update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,12 +5,17 @@
         background-color: #c1e0ef;
         padding: 20px 0;
     }
+    .cover-img{
+        width: 100%;
+    }
 </style>
 <div class="article-content">
     <div class="container">
         <div class="admin-card">
             <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a><br><br>
             <hr>
+            <img src="/cover/{{ $articles->cover }}" class="cover-img" alt="Cover Image">
+            <br><br>
             <h1>{{ $articles->article_name }}</h1>
         </div>
     </div>
