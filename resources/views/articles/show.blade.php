84c70ec951958734e89c449193123fcec89d3a94
JehanKandy <jehankandy@gmail.com>
2023-06-29 06:17:34 +0530

    Update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -11,7 +11,7 @@
         <div class="admin-card">
             <a href="{{ url('/articles') }}"><button class="btn btn-primary">Back</button></a><br><br>
             <hr>
-            
+            <h1>{{ $articles->article_name }}</h1>
         </div>
     </div>
 </div>
