8cd4e0340a6ec1b099d01fe9b88e9a1cfa905c2c
JehanKandy <jehankandy@gmail.com>
2023-06-29 06:28:00 +0530

    Update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -31,10 +31,10 @@
             </p>
             <div class="row">
                 <div class="col-lg-6">
-                    <span>Created at : {{ $articles->tags }}</span>
+                    <span>Created at : {{ $articles->created_at }}</span>
                 </div>
                 <div class="col-lg-6">
-                    <span>Last Update at : {{ $articles->tags }}</span>
+                    <span>Last Update at : {{ $articles->updated_at }}</span>
                 </div>
             </div>
         </div>
