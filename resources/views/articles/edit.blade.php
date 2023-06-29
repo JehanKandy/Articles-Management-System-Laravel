7a5e62b8742f749a2015fe3643d742804492f672
JehanKandy <jehankandy@gmail.com>
2023-06-29 06:30:42 +0530

    Update

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,25 @@
+@include('layouts.header')
+
+<style>
+    .article-content{
+        background-color: #c1e0ef;
+        padding: 20px 0;
+    }
+    .cover-img{
+        width: 100%;
+    }
+    .tag{
+        background-color: rgb(91, 91, 248);
+        color: white;
+        padding: 5px;
+        border-radius: 5px; 
+    }
+</style>
+
+<div class="article-content">
+    <div class="container">
+        <div class="admin-card">
+            
+        </div>
+    </div>
+</div>
